5687a14f73541043c4b8ad7714805fb7f6635800
Kevin Gomes <kevin.gomes.dev@gmail.com>
2023-01-06 09:59:57 -0500

    Adding config for tests and custReg and about

--- tests/functional/test_about.py
+++ tests/functional/test_about.py
@@ -1,14 +1,14 @@
 from app import app
 
 
-def testHome(testClient):
+def testAbout(testClient):
     '''
     GIVEN a Flask app configured for testing
-    WHEN the '/' page is requested
+    WHEN the '/about' page is requested
     THEN check that the response is valid and the method is GET
     '''
     
     response = testClient.get('/')
     assert response.request.method == 'GET'
     assert response.status_code == 200
-    assert b'Home page' in response.data
+    assert b'About' in response.data
--- tests/functional/test_custRegister.py
+++ tests/functional/test_custRegister.py
@@ -1,14 +1,14 @@
 from app import app
 
 
-def testHome(testClient):
+def testCustRegister(testClient):
     '''
     GIVEN a Flask app configured for testing
-    WHEN the '/' page is requested
-    THEN check that the response is valid and the method is GET
+    WHEN the '/custRegister' page is requested
+    THEN check that the response is valid
     '''
-    
+
     response = testClient.get('/')
     assert response.request.method == 'GET'
     assert response.status_code == 200
-    assert b'Home page' in response.data
+    assert b'Customer Registration' in response.data
